4f2e6f182851585efa212bf7e802df53ca8f147d
sebastian araya <sebastianalberto.araya@gmail.com>
2020-10-25 16:36:37 -0300

    planillafragment3 error
    codigo simple para listar objeto

--- proyecto/app/src/main/java/com/CDH/myapplication/ui/fragments/planillaFragment3.java
+++ proyecto/app/src/main/java/com/CDH/myapplication/ui/fragments/planillaFragment3.java
@@ -12,15 +12,22 @@ import androidx.navigation.Navigation;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import com.CDH.myapplication.R;
 import com.CDH.myapplication.ui.vistas.PlanillaFragment3ViewModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class planillaFragment3 extends Fragment {
+    
 
     private PlanillaFragment3ViewModel mViewModel;
-
+    
     public static planillaFragment3 newInstance() {
         return new planillaFragment3();
     }
@@ -37,6 +44,10 @@ public class planillaFragment3 extends Fragment {
         mViewModel = ViewModelProviders.of(this).get(PlanillaFragment3ViewModel.class);
         // TODO: Use the ViewModel
     }
+
+    private ListView lista;
+    List<String> items;
+    ArrayAdapter ADP;
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -62,5 +73,23 @@ public class planillaFragment3 extends Fragment {
                 Navigation.findNavController(v).navigate(R.id.planillaFragment2,bundle);
             }
         });
+        final EditText txtnombre = findViewById(R.id.txtnombre);
+        final EditText txtprecio = findViewById(R.id.txtprecio);
+        final Button btnagregar= findViewById(R.id.btnagregar);
+        lista = findViewById(R.id.lista);
+        items = new ArrayList<>();
+        ADP = new ArrayAdapter(getApplicationContext(),android.R.layout.simple_list_item_1,items);
+        lista.setAdapter(ADP);
+
+        btnagregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                items.add(txtnombre.getText().toString());
+                items.add(txtprecio.getText().toString());
+                ADP.notifyDataSetChanged();
+            }
+        });
     }
+
+
 }
