c5d4311b9a5cc7b49b2fbb78530ce5940bb7d572
sebastian araya <sebastianalberto.araya@gmail.com>
2020-11-05 03:26:54 -0300

    interfaz
    se modifico el diseño de la interfaz

---
+++ proyecto/app/src/main/java/com/CDH/myapplication/splash_activity.java
@@ -0,0 +1,21 @@
+package com.CDH.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splash_activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_activity);
+        new Handler().postDelayed(new Runnable() {
+            @Override public void run() {
+                Intent i = new Intent(splash_activity.this, MainActivity.class); startActivity(i);
+                finish(); } }, 3000);
+    }
+}
